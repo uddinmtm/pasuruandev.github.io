8e1f3fcb4fc3645dce3572a82bc4a54de54d2bde
imamsutono <imamssutono@gmail.com>
2018-10-08 07:29:47 +0700

    Smooth scroll onclick navbar menu

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 $('.carousel.carousel-slider').carousel({ fullWidth: true });
 $('.button-collapse').sideNav();
+
+function toSection(elementId) {
+  document.getElementById(elementId).scrollIntoView({
+    behavior: 'smooth',
+    inline: 'nearest'
+  });
+}
